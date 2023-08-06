3106e0df7bf3c9cd9c8fb5862893bb90e2f23483
Akash Pandya <91130000+akashpandya@users.noreply.github.com>
2023-08-06 15:11:17 -0400

    Version0.9

--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -2,7 +2,55 @@ import Footer from "../Footer";
 function AboutUs() {
   return (
     <div className="">
-      <div className="AboutUs">AboutUs</div>
+      <div className="AboutUs">
+        <div className="founders">
+          <img src="" alt="Founders_and_Goals" />
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolorem facilis deleniti saepe asperiores aliquid sed in at voluptatibus debitis pariatur!
+            Ut porro nulla adipisci voluptas, doloremque dolorem a. Itaque, aut?
+          </p>
+        </div>
+        <div className="founder_1">
+          <img src="" alt="Founder_1" />
+          <h1>Founder1</h1>
+          <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nihil eaque alias iste aspernatur quas numquam exercitationem necessitatibus? Reiciendis,
+            et quasi.
+          </p>
+        </div>
+        <div className="founder_2">
+          <img src="" alt="Founder_2" />
+          <h1>Founder2</h1>
+          <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nihil eaque alias iste aspernatur quas numquam exercitationem necessitatibus? Reiciendis,
+            et quasi.
+          </p>
+        </div>
+        <div className="founder_3">
+          <img src="" alt="Founder_3" />
+          <h1>Founder3</h1>
+          <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nihil eaque alias iste aspernatur quas numquam exercitationem necessitatibus? Reiciendis,
+            et quasi.
+          </p>
+        </div>
+        <div className="founder_4">
+          <img src="" alt="Founder_4" />
+          <h1>Founder4</h1>
+          <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nihil eaque alias iste aspernatur quas numquam exercitationem necessitatibus? Reiciendis,
+            et quasi.
+          </p>
+        </div>
+        <div className="founder_5">
+          <img src="" alt="Founder_5" />
+          <h1>Founder5</h1>
+          <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nihil eaque alias iste aspernatur quas numquam exercitationem necessitatibus? Reiciendis,
+            et quasi.
+          </p>
+        </div>
+      </div>
       <Footer />
     </div>
   );
--- src/pages/Events.js
+++ src/pages/Events.js
@@ -1,8 +1,83 @@
 import Footer from "../Footer";
+import "add-to-calendar-button";
+import "../App.css";
 function Events() {
   return (
     <div className="">
-      <div className="events">Events</div>
+      <div className="events">
+        <div className="event1">
+          <img src="\images\img_event_1.jpg" alt="img_event_1" className="images_events" />
+          <h1>RangTarang Senior' Club</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam voluptatum quis unde eveniet tempore fugit cupiditate illum. Unde, quo. Corporis
+            ratione vitae reiciendis delectus, consectetur quis asperiores voluptate? Dolor, nam?
+          </p>
+          <add-to-calendar-button
+            name="Title"
+            options="'Apple','Google'"
+            location="World Wide Web"
+            startDate="2023-08-07"
+            endDate="2023-08-07"
+            startTime="10:15"
+            endTime="23:30"
+            timeZone="America/Los_Angeles"
+          ></add-to-calendar-button>
+        </div>
+        <div className="event2">
+          <img src="\images\img_event_2.jpg" alt="img_event_2" className="images_events" />
+          <h1>RangTarang Karaoke Night</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam voluptatum quis unde eveniet tempore fugit cupiditate illum. Unde, quo. Corporis
+            ratione vitae reiciendis delectus, consectetur quis asperiores voluptate? Dolor, nam?
+          </p>
+          <add-to-calendar-button
+            name="Title"
+            options="'Apple','Google'"
+            location="World Wide Web"
+            startDate="2023-08-07"
+            endDate="2023-08-07"
+            startTime="10:15"
+            endTime="23:30"
+            timeZone="America/Los_Angeles"
+          ></add-to-calendar-button>
+        </div>
+        <div className="event3">
+          <img src="\images\img_event_3.jpg" alt="img_event_3" className="images_events" />
+          <h1>RangTarang Kids' Club</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam voluptatum quis unde eveniet tempore fugit cupiditate illum. Unde, quo. Corporis
+            ratione vitae reiciendis delectus, consectetur quis asperiores voluptate? Dolor, nam?
+          </p>
+          <add-to-calendar-button
+            name="Title"
+            options="'Apple','Google'"
+            location="World Wide Web"
+            startDate="2023-08-07"
+            endDate="2023-08-07"
+            startTime="10:15"
+            endTime="23:30"
+            timeZone="America/Los_Angeles"
+          ></add-to-calendar-button>
+        </div>
+        <div className="event4">
+          <img src="\images\img_event_4.jpg" alt="img_event_2" className="images_events" />
+          <h1>RangTarang Ladies' Club</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam voluptatum quis unde eveniet tempore fugit cupiditate illum. Unde, quo. Corporis
+            ratione vitae reiciendis delectus, consectetur quis asperiores voluptate? Dolor, nam?
+          </p>
+          <add-to-calendar-button
+            name="Title"
+            options="'Apple','Google'"
+            location="World Wide Web"
+            startDate="2023-08-07"
+            endDate="2023-08-07"
+            startTime="10:15"
+            endTime="23:30"
+            timeZone="America/Los_Angeles"
+          ></add-to-calendar-button>
+        </div>
+      </div>
       <Footer />
     </div>
   );

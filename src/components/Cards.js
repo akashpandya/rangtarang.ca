fe51b07784621a36e023bc6ba34be78eb32e7ed7
akashpandya <akash2pandya@gmail.com>
2024-08-28 13:33:22 -0400

    Changed images

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -8,9 +8,9 @@ function Cards() {
       <div className="cards__container">
         <div className="cards__wrapper">
           <ul className="cards__items">
-            <CardItem src="images/family_event.png" text="Join now!" label="RangTarang Summer Picnic 2024" path="/Events" />
+            <CardItem src="images/Navratri2024" text="Join now!" label="RangTarang Navratri Event 2024" path="/Events" />
           </ul>
-          <h1>RangTarang Clubs</h1>
+          <h1>RangTarang Navratri 2024</h1>
           <ul className="cards__items">
             <CardItem src="images/karaoke.jpg" text="Join us for monthly Karaoke nights!" label="karaoke Club" path="/Karaokeclub" />
             <CardItem src="images/kids.jpg" text="Where Oshawa's young explorers embark on endless adventures!" label="Kids Club" path="/kidsclub" />
